0933ed8f356c54387466a081b72cc5777c6a2818
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-09-16 17:05:46 +0100

    remove unused Routers import from Layout component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./../Header/Header";
-import Routers from "../../router/Routers";
 import Footer from "./../Footer/Footer";
 import { Outlet } from "react-router";
 
